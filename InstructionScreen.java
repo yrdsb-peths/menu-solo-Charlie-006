1dfce064d41eeea6cd14967e0e771c33b5f5edbb
Charlie-006 <440028963@gapps.yrdsb.ca>
2024-10-15 00:19:49 -0400

    added two button

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -5,6 +5,10 @@ public class InstructionScreen extends World {
   public InstructionScreen(World previousWorld) {
         super(600, 400, 1);
         this.previousWorld = previousWorld;
+
+
+        addObject(new Button(this::nextInstruction), 500, 340);
+        addObject(new Button(this::previousInstruction), 100, 340);
     }
 
   public void goPrevious() {
