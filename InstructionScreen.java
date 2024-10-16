3cc49f890aa8bac888848a12345c6bfbb29c32ae
Charlie-006 <440028963@gapps.yrdsb.ca>
2024-10-16 12:15:48 -0400

    update

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -6,11 +6,9 @@ public class InstructionScreen extends World {
         super(600, 400, 1);
         this.previousWorld = previousWorld;
 
-        Button ButtonNext = new Button();
-        Button ButtonPrevious = new Button();
 
-        addObject(ButtonNext, 500, 340);
-        addObject(ButtonPrevious, 100, 340);
+        addObject(new Button(this::nextInstruction), 500, 340);
+        addObject(new Button(this::previousInstruction), 100, 340);
     }
 
   public void goPrevious() {
