93c003c57686ff85e5179355a29f642017c55fe0
Charlie-006 <440028963@gapps.yrdsb.ca>
2024-10-17 13:19:37 -0400

    added the instructionScreen to the world

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,41 +1,46 @@
 import greenfoot.*;
 
 public class InstructionScreen extends World {
-  private World previousWorld;
-  private String[] instructions = {
-    "Welcome to the game!",
-    "Use arrow keys to move.",
-    "Press space to jump."
-  };
+    private World previousWorld;
+    private String[] instructions = {
+        "Welcome to the game!",
+        "Use arrow keys to move.",
+        "Press space to jump."
+    };
 
-  private Label instructionLabel;
-  private int Index = 0;
+    private Label instructionLabel;
+    private int index = 0;
 
-  public InstructionScreen(World previousWorld) {
+    public InstructionScreen(World previousWorld) {
         super(600, 400, 1);
         this.previousWorld = previousWorld;
-        instructionLabel = new Label(instructions[Index], 30);
+
+        // Create a label with the first instruction
+        instructionLabel = new Label(instructions[index], 30);
         
+        // Add the instruction label to the screen
         addObject(instructionLabel, 300, 200);
-        addObject(new Button(this::nextInstruction, "Next"), 500, 340);
-        addObject(new Button(this::previousInstruction, "Previous"), 100, 340);
-  }
-  
-  public void nextInstruction() {
-    if (Index < instructions.length - 1) {
-      Index++;
-      instructionLabel.setValue(instructions[Index]);
+
+        // Add Next and Previous buttons
+        addObject(new Button(this::nextInstruction, "Next"), 450, 340);
+        addObject(new Button(this::previousInstruction, "Previous"), 130, 340);
     }
-  }
-  
-  public void previousInstruction() {
-    if (Index > 0) {
-      Index--;
-      instructionLabel.setValue(instructions[Index]);
-  }
-  }
-  
-  public void goPrevious() {
-      Greenfoot.setWorld(previousWorld);;
-  }
+
+    public void nextInstruction() {
+        if (index < instructions.length - 1) {
+            index++;
+            instructionLabel.setValue(instructions[index]); // Update the label with the next instruction
+        }
+    }
+
+    public void previousInstruction() {
+        if (index > 0) {
+            index--;
+            instructionLabel.setValue(instructions[index]); // Update the label with the previous instruction
+        }
+    }
+
+    public void goPrevious() {
+        Greenfoot.setWorld(previousWorld);
+    }
 }
