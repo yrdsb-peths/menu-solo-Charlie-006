60669a4077f69e93d709c5daa0251e72a732cad9
Charlie-006 <440028963@gapps.yrdsb.ca>
2024-10-21 22:58:46 -0400

    added the "Goto" menuScreen button

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -24,6 +24,8 @@ public class InstructionScreen extends World {
         // Add Next and Previous buttons
         addObject(new Button(this::nextInstruction, "Next"), 450, 340);
         addObject(new Button(this::previousInstruction, "Previous"), 130, 340);
+
+        addObject(new Button(this :: goPrevious, "Menu"), 250, 340);
     }
 
     public void nextInstruction() {
